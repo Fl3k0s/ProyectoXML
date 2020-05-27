39bb5006bb64a17a9c95d6e2bb665b0be122ed24
Flekos <65820449+Fl3k0s@users.noreply.github.com>
2020-05-27 14:55:25 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function activarwifi(){
+    
+
+
+}
+
+
+function Inicializar(){
+    document.getElementById('act').addEventListener('click', activarwifi);
+    document.getElementById('act').addEventListener('click', cerrar);
+}
+    
+document.addEventListener('DOMContentLoaded',Inicializar,false);
